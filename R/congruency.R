20a8d2b6c34298d2717e4777a2f757d8ac6b0fc1
Wainright <cwainright@nps.gov>
2026-05-07 09:01:49 -0400

    [refactor] abstracting so that congruency checks will scale to NCRNWater package

--- R/congruency.R
+++ R/congruency.R
@@ -1,181 +1,213 @@
 
-check_congruency <- function(data_filename, metadata_filename) {
+check_congruency <- function(data_filename, metadata_filename, data_template='templates/wqp_template.csv', metadata_template = 'templates/wqp_ncrnwater_metadata_template.csv') {
   
-  problems <- 0
+  cat(paste0('Checking congruency between ', data_filename, ' and ', metadata_filename, ' ...\n\n'))
+  
+  # package together for convenience
+  files <- c(data_filename, metadata_filename)
+  
+  # instantiate
+  results <- list()
+  results[['problems']] <- 0
+  results[['msgs']] <- c()
   
   # do the files exist?
+  tmp <- check_congruency_files_exist(files)
+  results[['problems']] <- results[['problems']] + tmp[['problems']]
+  results[['msgs']] <- c(results[['msgs']], tmp[['msgs']])
 
-  if (file.exists(data_filename)==F){
-    msg_beginning <- "'" 
-    msg_end <- ' does not exist. Check for typos or missing information and try again.'
-    msg <- paste0(msg_beginning, data_filename, msg_end)
-    problems <- problems + 1
-    stop(msg)
-  } else {
-    msg_beginning <- ""
-    msg_end <- ' exists'
-    msg <- paste0(msg_beginning, data_filename, msg_end, '\n')
-    cat(msg)
-  }
-  if (file.exists(metadata_filename)==F){
-    msg <- paste0(msg_beginning, metadata_filename, msg_end)
-    problems <- problems + 1
-    stop(msg)
-  } else {
-    msg_beginning <- ""
-    msg_end <- ' exists'
-    msg <- paste0(msg_beginning, metadata_filename, msg_end, '\n')
-    cat(msg)
-  }
-  
   # are the files csv?
-  msg_beginning <- "'"
-  msg_end <- ' is not a CSV file. Check for typos or missing information and try again.'
-  if (endsWith(data_filename, '.csv')==F){
-    msg <- paste0(msg_beginning, data_filename, msg_end)
-    problems <- problems + 1
-    stop(msg)
-  } else {
-    msg_beginning <- ""
-    msg_end <- ' is a CSV'
-    msg <- paste0(msg_beginning, data_filename, msg_end, '\n')
-    cat(msg)
-  }
-  if (endsWith(metadata_filename, '.csv')==F){
-    msg <- paste0(msg_beginning, metadata_filename, msg_end)
-    problems <- problems + 1
-    stop(msg)
-  } else {
-    msg_beginning <- ""
-    msg_end <- ' is a CSV'
-    msg <- paste0(msg_beginning, metadata_filename, msg_end, '\n')
-    cat(msg)
-  }
-  
-  data <- read.csv(data_filename)
-  metadata <- read.csv(metadata_filename)
-  wqp_template <- read.csv('templates/wqp_template.csv')
-  metadata_template <- read.csv('templates/wqp_ncrnwater_metadata_template.csv')
-  
-  # ---------- do the files match the format we expect? ---------- #
-  # are the columns present and named properly?
-  msg_beginning <- "The column names in '" 
-  msg_end <- ' do not match the required format.'
-  
-  if (all(colnames(wqp_template) != colnames(data))){
-    problems <- problems + 1
-    msg <- paste0(msg_beginning, data_filename, msg_end)
-    warning(msg)
-    in_template_not_data <- which(colnames(wqp_template) %in% colnames(data) == F)
-    in_data_not_template <- which(colnames(data) %in% colnames(wqp_template) == F)
-    if (length(in_template_not_data)>0){
-      msg_beginning <- paste0("These columns are in WQP format but not in ",data_filename,":\n")
-      msg_end <- "\n"
-      msg <- paste0(msg_beginning, paste(in_template_not_data, collapse = ', '), msg_end)
-      warning(msg)
-    }
-    if (length(in_data_not_template)>0){
-      msg_beginning <- paste0("These columns are in ",data_filename," but not in WQP format:\n")
-      msg_end <- "\n"
-      msg <- paste0(msg_beginning, paste(in_data_not_template, collapse = ', '), msg_end)
-      warning(msg)
-    }
-    stop()
-  } else {
-    msg_beginning <- "All column names in "
-    msg_end <- ' match WQP format'
-    msg <- paste0(msg_beginning, data_filename, msg_end, '\n')
-    cat(msg)
-  }
-  if (all(colnames(metadata_template) != colnames(metadata))){
-    problems <- problems + 1
-    msg <- paste0(msg_beginning, data_filename, msg_end)
-    warning(msg)
-    in_template_not_data <- which(colnames(metadata_template) %in% colnames(metadata) == F)
-    in_data_not_template <- which(colnames(metadata) %in% colnames(metadata_template) == F)
-    if (length(in_template_not_data)>0){
-      msg_beginning <- paste0("These columns are in NCRNWater metadata but not in ",metadata_filename,":\n")
-      msg_end <- "\n"
-      msg <- paste0(msg_beginning, paste(in_template_not_data, collapse = ', '), msg_end)
-      warning(msg)
-    }
-    if (length(in_data_not_template)>0){
-      msg_beginning <- paste0("These columns are in ",metadata_filename," but not in NCRNWater metadata:\n")
-      msg_end <- "\n"
-      msg <- paste0(msg_beginning, paste(in_data_not_template, collapse = ', '), msg_end)
-      warning(msg)
-    }
-    stop()
-  } else {
-    msg_beginning <- "All column names in "
-    msg_end <- ' match NCRNWater metadata format'
-    msg <- paste0(msg_beginning, metadata_filename, msg_end, '\n')
-    cat(msg)
-  }
-  
-  # are sites present and named properly?
-  msg_beginning <- "The site names in '" 
-  msg_end <- "' do not match."
-  if (all(data$MonitoringLocationName %>% unique %in% metadata$SiteName %>% unique) == F){
-    problems <- problems + 1
-    msg <- paste0(msg_beginning, data_filename, msg_end)
-    warning(msg)
-    data_locs <- data$MonitoringLocationName %>% unique
-    metadata_locs <- metadata$SiteName %>% unique
-    in_metadata_not_data <- which(colnames(data_locs) %in% colnames(metadata_locs) == F)
-    in_data_not_metadata <- which(colnames(metadata_locs) %in% colnames(data_locs) == F)
-    if (length(in_metadata_not_data)>0){
-      msg_beginning <- paste0("These site names are in ",metadata_filename," but not in ",data_filename,":\n")
-      msg_end <- "\n"
-      msg <- paste0(msg_beginning, paste(in_metadata_not_data, collapse = ', '), msg_end)
-      warning(msg)
-    }
-    if (length(in_data_not_metadata)>0){
-      msg_beginning <- paste0("These site names are in ",data_filename," but not in ",metadata_filename,":\n")
-      msg_end <- "\n"
-      msg <- paste0(msg_beginning, paste(in_data_not_metadata, collapse = ', '), msg_end)
-      warning(msg)
-    }
-  } else {
-    msg_beginning <- "All site names in "
-    msg_end <- ' match the site names in '
-    msg <- paste0(msg_beginning, data_filename, msg_end, metadata_filename, '\n')
-    cat(msg)
-  }
+  tmp <- check_congruency_files_are_csvs(files)
+  results[['problems']] <- results[['problems']] + tmp[['problems']]
+  results[['msgs']] <- c(results[['msgs']], tmp[['msgs']])
 
-    # are sites present and named properly?
-  msg_beginning <- "The site codes in '" 
-  msg_end <- "' do not match."
-  if (all(data$MonitoringLocationIdentifier %>% unique %in% metadata$SiteCode %>% unique) == F){
-    problems <- problems + 1
-    msg <- paste0(msg_beginning, data_filename, msg_end)
-    warning(msg)
-    data_locs <- data$MonitoringLocationIdentifier %>% unique
-    metadata_locs <- metadata$SiteCode %>% unique
-    in_metadata_not_data <- which(colnames(data_locs) %in% colnames(metadata_locs) == F)
-    in_data_not_metadata <- which(colnames(metadata_locs) %in% colnames(data_locs) == F)
-    if (length(in_metadata_not_data)>0){
-      msg_beginning <- paste0("These site codes are in ",metadata_filename," but not in ",data_filename,":\n")
-      msg_end <- "\n"
-      msg <- paste0(msg_beginning, paste(in_metadata_not_data, collapse = ', '), msg_end)
-      warning(msg)
-    }
-    if (length(in_data_not_metadata)>0){
-      msg_beginning <- paste0("These site codes are in ",data_filename," but not in ",metadata_filename,":\n")
-      msg_end <- "\n"
-      msg <- paste0(msg_beginning, paste(in_data_not_metadata, collapse = ', '), msg_end)
-      warning(msg)
-    }
+  # if the files exist and are the correct filetype, we're ready to test the files themselves
+  
+  files <- list(
+    'user_files' = list(
+      'data' = read.csv(data_filename)
+      ,'metadata' = read.csv(metadata_filename)
+    )
+    ,'template_files' = list(
+      'data' = read.csv(data_template)
+      'metadata' = read.csv(metadata_template)
+    )
+  )
+  # 
+  # # ---------- do the files match the format we expect? ---------- #
+  # # are the columns present and named properly?
+  # msg_beginning <- "The column names in '" 
+  # msg_end <- ' do not match the required format.'
+  # 
+  # if (all(colnames(wqp_template) != colnames(data))){
+  #   problems <- problems + 1
+  #   msg <- paste0(msg_beginning, data_filename, msg_end)
+  #   warning(msg)
+  #   in_template_not_data <- which(colnames(wqp_template) %in% colnames(data) == F)
+  #   in_data_not_template <- which(colnames(data) %in% colnames(wqp_template) == F)
+  #   if (length(in_template_not_data)>0){
+  #     msg_beginning <- paste0("These columns are in WQP format but not in ",data_filename,":\n")
+  #     msg_end <- "\n"
+  #     msg <- paste0(msg_beginning, paste(in_template_not_data, collapse = ', '), msg_end)
+  #     warning(msg)
+  #   }
+  #   if (length(in_data_not_template)>0){
+  #     msg_beginning <- paste0("These columns are in ",data_filename," but not in WQP format:\n")
+  #     msg_end <- "\n"
+  #     msg <- paste0(msg_beginning, paste(in_data_not_template, collapse = ', '), msg_end)
+  #     warning(msg)
+  #   }
+  #   stop()
+  # } else {
+  #   msg_beginning <- "All column names in "
+  #   msg_end <- ' match WQP format'
+  #   msg <- paste0(msg_beginning, data_filename, msg_end, '\n')
+  #   cat(msg)
+  # }
+  # if (all(colnames(metadata_template) != colnames(metadata))){
+  #   problems <- problems + 1
+  #   msg <- paste0(msg_beginning, data_filename, msg_end)
+  #   warning(msg)
+  #   in_template_not_data <- which(colnames(metadata_template) %in% colnames(metadata) == F)
+  #   in_data_not_template <- which(colnames(metadata) %in% colnames(metadata_template) == F)
+  #   if (length(in_template_not_data)>0){
+  #     msg_beginning <- paste0("These columns are in NCRNWater metadata but not in ",metadata_filename,":\n")
+  #     msg_end <- "\n"
+  #     msg <- paste0(msg_beginning, paste(in_template_not_data, collapse = ', '), msg_end)
+  #     warning(msg)
+  #   }
+  #   if (length(in_data_not_template)>0){
+  #     msg_beginning <- paste0("These columns are in ",metadata_filename," but not in NCRNWater metadata:\n")
+  #     msg_end <- "\n"
+  #     msg <- paste0(msg_beginning, paste(in_data_not_template, collapse = ', '), msg_end)
+  #     warning(msg)
+  #   }
+  #   stop()
+  # } else {
+  #   msg_beginning <- "All column names in "
+  #   msg_end <- ' match NCRNWater metadata format'
+  #   msg <- paste0(msg_beginning, metadata_filename, msg_end, '\n')
+  #   cat(msg)
+  # }
+  # 
+  # # are sites present and named properly?
+  # msg_beginning <- "The site names in '" 
+  # msg_end <- "' do not match."
+  # if (all(data$MonitoringLocationName %>% unique %in% metadata$SiteName %>% unique) == F){
+  #   problems <- problems + 1
+  #   msg <- paste0(msg_beginning, data_filename, msg_end)
+  #   warning(msg)
+  #   data_locs <- data$MonitoringLocationName %>% unique
+  #   metadata_locs <- metadata$SiteName %>% unique
+  #   in_metadata_not_data <- which(colnames(data_locs) %in% colnames(metadata_locs) == F)
+  #   in_data_not_metadata <- which(colnames(metadata_locs) %in% colnames(data_locs) == F)
+  #   if (length(in_metadata_not_data)>0){
+  #     msg_beginning <- paste0("These site names are in ",metadata_filename," but not in ",data_filename,":\n")
+  #     msg_end <- "\n"
+  #     msg <- paste0(msg_beginning, paste(in_metadata_not_data, collapse = ', '), msg_end)
+  #     warning(msg)
+  #   }
+  #   if (length(in_data_not_metadata)>0){
+  #     msg_beginning <- paste0("These site names are in ",data_filename," but not in ",metadata_filename,":\n")
+  #     msg_end <- "\n"
+  #     msg <- paste0(msg_beginning, paste(in_data_not_metadata, collapse = ', '), msg_end)
+  #     warning(msg)
+  #   }
+  # } else {
+  #   msg_beginning <- "All site names in "
+  #   msg_end <- ' match the site names in '
+  #   msg <- paste0(msg_beginning, data_filename, msg_end, metadata_filename, '\n')
+  #   cat(msg)
+  # }
+  # 
+  #   # are sites present and named properly?
+  # msg_beginning <- "The site codes in '" 
+  # msg_end <- "' do not match."
+  # if (all(data$MonitoringLocationIdentifier %>% unique %in% metadata$SiteCode %>% unique) == F){
+  #   problems <- problems + 1
+  #   msg <- paste0(msg_beginning, data_filename, msg_end)
+  #   warning(msg)
+  #   data_locs <- data$MonitoringLocationIdentifier %>% unique
+  #   metadata_locs <- metadata$SiteCode %>% unique
+  #   in_metadata_not_data <- which(colnames(data_locs) %in% colnames(metadata_locs) == F)
+  #   in_data_not_metadata <- which(colnames(metadata_locs) %in% colnames(data_locs) == F)
+  #   if (length(in_metadata_not_data)>0){
+  #     msg_beginning <- paste0("These site codes are in ",metadata_filename," but not in ",data_filename,":\n")
+  #     msg_end <- "\n"
+  #     msg <- paste0(msg_beginning, paste(in_metadata_not_data, collapse = ', '), msg_end)
+  #     warning(msg)
+  #   }
+  #   if (length(in_data_not_metadata)>0){
+  #     msg_beginning <- paste0("These site codes are in ",data_filename," but not in ",metadata_filename,":\n")
+  #     msg_end <- "\n"
+  #     msg <- paste0(msg_beginning, paste(in_data_not_metadata, collapse = ', '), msg_end)
+  #     warning(msg)
+  #   }
+  # } else {
+  #   msg_beginning <- "All site codes in "
+  #   msg_end <- ' match the site codes in '
+  #   msg <- paste0(msg_beginning, data_filename, msg_end, metadata_filename, '\n')
+  #   cat(msg)
+  # }
+  # 
+  
+  msgs <- paste(results[['msgs']], collapse = '\n')
+  if (results$problems == 0){
+    cat(msgs)
+    cat('\n\nOK to proceed!\n')
   } else {
-    msg_beginning <- "All site codes in "
-    msg_end <- ' match the site codes in '
-    msg <- paste0(msg_beginning, data_filename, msg_end, metadata_filename, '\n')
-    cat(msg)
+    cat(paste0('Your files have ',results[['problems']],' congruency problems. Resolve those problems before proceeding.'))
+    cat(msgs)
+    stop()
   }
   
-  if (problems == 0){
-    cat('\nOK to proceed!\n')
-  }
+}
+
+check_congruency_columns_match <- function(files) {
   
+}
+
+check_congruency_files_exist <- function(files) {
+  tmp <- list(
+    'msgs' = c()
+    ,problems = 0
+  )
+  
+  for (f in files){
+    if (file.exists(f)==F){
+      msg_beginning <- "'" 
+      msg_end <- ' does not exist. Check for typos or missing information and try again.'
+      msg <- paste0(msg_beginning, f, msg_end)
+      tmp[['problems']] <- tmp[['problems']] + 1
+    } else {
+      msg_beginning <- ""
+      msg_end <- ' exists'
+      msg <- paste0(msg_beginning, f, msg_end)
+    }
+    tmp[['msgs']] <- c(tmp[['msgs']], msg)
+  }
+  return(tmp)
+}
+
+check_congruency_files_are_csvs <- function(files) {
+  tmp <- list(
+    'msgs' = c()
+    ,problems = 0
+  )
+  
+  for (f in files){
+    if (endsWith(f, '.csv')==F){
+      msg_beginning <- "'" 
+      msg_end <- ' is not a CSV file. Check for typos or missing information and try again.'
+      msg <- paste0(msg_beginning, f, msg_end)
+      tmp[['problems']] <- tmp[['problems']] + 1
+    } else {
+      msg_beginning <- ""
+      msg_end <- ' is a CSV'
+      msg <- paste0(msg_beginning, f, msg_end)
+    }
+    tmp[['msgs']] <- c(tmp[['msgs']], msg)
+  }
+  return(tmp)
 }
 
